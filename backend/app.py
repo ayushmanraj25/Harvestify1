d8fc3b7ac647fa395ff9f2aa61c7c272d5ee590a
ayushmanraj25 <ayushmanraj430@gmail.com>
2026-06-01 19:22:41 +0530

    Updated  UI Updated

--- backend/app.py
+++ backend/app.py
@@ -694,4 +694,3 @@ Context: {feature_context}"""
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5003)), debug=True)
-
